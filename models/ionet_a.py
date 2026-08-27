cc33227ee8b6cd5827f9cfb0768927dc3e45117b
keepthebasic <31949618+keepthebasic@users.noreply.github.com>
2026-08-27 17:02:58 +0800

    Add files via upload

---
+++ models/ionet_a.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+from models.ionet_lite import IONetLite as IONetA, IONetLiteConfig as IONetAConfig, build_ionet_lite as build_ionet_a, ionet_lite_loss as ionet_a_loss
+__all__ = ['IONetA', 'IONetAConfig', 'build_ionet_a', 'ionet_a_loss']
